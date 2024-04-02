07432e33e43dfaa9709e1554a6617c969d688240
Wen-Xin <u10816006@go.utaipei.edu.tw>
2024-04-03 00:02:31 +0800

    調整server action file path，稍微修改內容加上TODO

---
+++ src/app/_actions/issue/createIssue.ts
@@ -0,0 +1,24 @@
+// TODO:
+import { Octokit } from "octokit";
+
+type Props = {
+  token: string;
+  page: number;
+  pageSize?: number;
+};
+
+export default async function createIssue({ token, page, pageSize = 5 }: Props) {
+  "use server";
+  const octokit = new Octokit({
+    auth: token,
+  });
+
+  const resp = await octokit.rest.issues.list({
+    owner: "seallalalalal",
+    repo: "github-issue",
+    page: page,
+    per_page: pageSize,
+  });
+
+  return resp.data;
+}
---
+++ src/app/_actions/issue/getIssueList.ts
@@ -0,0 +1,23 @@
+import { Octokit } from "octokit";
+
+type Props = {
+  token: string;
+  page: number;
+  pageSize?: number;
+};
+
+export default async function getIssueList({ token, page, pageSize = 5 }: Props) {
+  "use server";
+  const octokit = new Octokit({
+    auth: token,
+  });
+
+  const resp = await octokit.rest.issues.list({
+    owner: "seallalalalal",
+    repo: "github-issue",
+    page: page,
+    per_page: pageSize,
+  });
+
+  return resp.data;
+}
