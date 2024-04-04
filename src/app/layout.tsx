b2ed1d6b49dcc8e364c1f7cb71195da0f918ee7e
Wen-Xin <u10816006@go.utaipei.edu.tw>
2024-04-04 23:26:24 +0800

    Feat: add Navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <SessionProvider session={session}>
             <QueryProvider>
               <NextUIProvider>
-                {/* <NavBar /> */}
+                <NavBar />
                 {children}
               </NextUIProvider>
             </QueryProvider>
