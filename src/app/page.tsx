7cdd8c1e6be955cd70db53d36558374a691f74b8
Wen-Xin <u10816006@go.utaipei.edu.tw>
2024-04-06 01:30:40 +0800

    Feat: redirect to /list if Sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
-import { Button } from "@nextui-org/react";
-import styles from "./page.module.css";
 import { getServerSession } from "next-auth";
 import { handler } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession(handler);
-  console.log({ session });
+
+  if (!session) {
+    return (
+      <div className="flex h-full w-full items-center justify-center text-large">
+        Click "Sign In" button to continue.
+      </div>
+    );
+  }
+
+  if (session) {
+    redirect("/list");
+  }
 
   return (
-    <main className={styles.main}>
+    <>
       <div>
-        Home page
-        {JSON.stringify(session, null, 2)}
+        <div>Unknown Error</div>
+        <div>Try Sign In again later</div>
+        <div>{JSON.stringify(session, null, 2)}</div>
       </div>
-    </main>
+    </>
   );
 }
