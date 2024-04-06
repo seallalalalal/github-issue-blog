1723486f4bd7314a734e0aae380ab9cc1dfafe5d
Wen-Xin <u10816006@go.utaipei.edu.tw>
2024-04-07 02:02:12 +0800

    Feat: update hook

---
+++ src/service/issue/useIssueMutateUpdate.ts
@@ -0,0 +1,39 @@
+"use client";
+import { REPO_NAME, REPO_OWNER } from "@/const/general";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Octokit } from "octokit";
+import { octokitCaller } from "../Octokit";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  title: string;
+  body: string;
+  number: number;
+  token: string;
+};
+
+async function updateIssue({ title, body, number, token }: Props) {
+  const octokit = new Octokit({ auth: token });
+
+  const resp = await octokit.rest.issues.update({
+    owner: REPO_OWNER,
+    repo: REPO_NAME,
+    title,
+    body,
+    issue_number: number,
+  });
+
+  return resp.data;
+}
+
+export default function useIssueMutateUpdate() {
+  const router = useRouter();
+  const query = useMutation({
+    mutationKey: ["updateIssue"],
+    mutationFn: updateIssue,
+    onSuccess(data, variables, context) {
+      router.push(`/list/${data.number}`);
+    },
+  });
+  return query;
+}
