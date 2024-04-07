b704f584201de5af6ee5e28f49376618eaee4b26
Wen-Xin <u10816006@go.utaipei.edu.tw>
2024-04-07 19:46:54 +0800

    Feat: infinite scroll list query

---
+++ src/service/issue/useIssuListQuery.ts
@@ -0,0 +1,63 @@
+"use client";
+import { REPO_NAME, REPO_OWNER } from "@/const/general";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { octokitCaller } from "../Octokit";
+
+import parse from "parse-link-header";
+import { IssueArray, issueList, IssueModel } from "../schema";
+
+type Props = {
+  page: number;
+  pageSize?: number;
+};
+
+type MetaType = { first: parse.Link | null; next: parse.Link | null; last: parse.Link | null };
+
+async function getIssueList({ page, pageSize = 10 }: Props & {}) {
+  const octokit = await octokitCaller();
+
+  const resp = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+    owner: REPO_OWNER,
+    repo: REPO_NAME,
+    page,
+    per_page: pageSize,
+  });
+
+  const link = parse(resp.headers.link);
+
+  const meta: MetaType = { first: null, next: null, last: null };
+
+  console.log({ link });
+  if (link?.last) {
+    meta.last = link.last;
+  }
+  if (link?.first) {
+    meta.first = link.first;
+  }
+  if (link?.next) {
+    meta.next = link.next;
+  }
+
+  return { data: IssueArray.parse(resp.data), meta };
+}
+
+export default function useIssueListQuery({ page, pageSize = 10 }: Props) {
+  const query = useInfiniteQuery({
+    queryKey: ["getIssueList", page, pageSize],
+    queryFn: ({ pageParam }) =>
+      getIssueList({
+        page: pageParam as number,
+        pageSize,
+      }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      const { next, first, last } = lastPage.meta;
+      if (!last) return undefined;
+      return Number(next?.page);
+    },
+    gcTime: 0,
+    placeholderData: (previousData, previousQuery) => previousData,
+  });
+
+  return query;
+}
